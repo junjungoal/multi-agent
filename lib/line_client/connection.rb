ce2b411f582152e0d6e21eb0685605a4f0605703
山田　潤 <junyamada107@gmail.com>
2016-04-12 02:25:31 +0900

    change method for uri

--- lib/line_client/connection.rb
+++ lib/line_client/connection.rb
@@ -5,9 +5,8 @@ module LineClient
     end
 
     def post(attributes={})
-      _req = Net::HTTP::Post.new(@uri.request_uri) 
+      _req = Net::HTTP::Post.new(@uri.path) 
       _req.set_content_type("application/json; charset=UTF-8")
-      _req['Content-type'] = 'application/json; charset=UTF-8'
       _req['X-Line-ChannelID'] = ENV["LINE_CHANNEL_ID"]
       _req['X-Line-ChannelSecret'] = ENV["LINE_CHANNEL_SECRET"]
       _req['X-Line-Trusted-User-With-ACL'] = ENV["LINE_CHANNEL_MID"]
