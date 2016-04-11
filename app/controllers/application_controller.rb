40e848f6825457630ae9cc199889e67b26596219
山田　潤 <junyamada107@gmail.com>
2016-04-11 19:40:43 +0900

    add validate signnature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  before_action :validate_signature
+
+  private
+
+  def is_validate_signature
+    signature = request.headers["X-LINE-ChannelSignature"]
+    http_request_body = request.raw_post
+    hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, CHANNEL_SECRET, http_request_body)
+    signature_answer = Base64.strict_encode64(hash)
+    signature == signature_answer
+  end
+
+  def validate_signature
+    render :nothing => true, status: 470 unless is_validate_signature
+  end
 end
