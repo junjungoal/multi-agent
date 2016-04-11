5e5c919aa98c2720b23ae93f13f760e550a495e3
山田　潤 <junyamada107@gmail.com>
2016-04-11 21:04:12 +0900

    fix type miss

--- lib/line_client/connection.rb
+++ lib/line_client/connection.rb
@@ -7,7 +7,7 @@ module LineClient
     def post(attributes={})
       _req = Net::HTTP::Post.new("https://trialbot-api.line.me") 
       _req.set_content_type("application/json; charset=UTF-8")
-      _req.headers['Content-type'] = 'application/json; charset=UTF-8'
+      _req['Content-type'] = 'application/json; charset=UTF-8'
       _req['X-Line-ChannelID'] = ENV["LINE_CHANNEL_ID"]
       _req['X-Line-ChannelSecret'] = ENV["LINE_CHANNEL_SECRET"]
       _req['X-Line-Trusted-User-With-ACL'] = ENV["LINE_CHANNEL_MID"]
