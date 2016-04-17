64a3301ffdcd27837df1453239e8858455e27dc0
山田　潤 <junyamada107@gmail.com>
2016-04-17 16:24:22 +0900

    fix typo

--- app/services/yelp_message.rb
+++ app/services/yelp_message.rb
@@ -1,7 +1,7 @@
 class YelpMessage
   def self.build(res)
     res.businesses.map do |content|
-      "名前 : #{content.name} \n レート: #{cotent.rating} \n url: #{conent.url}"
+      "名前 : #{content.name} \n レート: #{content.rating} \n url: #{content.url}"
     end
   end
 end
