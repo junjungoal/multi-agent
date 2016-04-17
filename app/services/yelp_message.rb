d885c7aaaa01d8833ae129eb2d0dbcaa249cfad0
山田　潤 <junyamada107@gmail.com>
2016-04-18 00:11:28 +0900

    delete output times method

--- app/services/yelp_message.rb
+++ app/services/yelp_message.rb
@@ -3,17 +3,10 @@ class YelpMessage
     res.businesses.map do |content|
      {  
       contentType: LineContent::TEXT,
-      text: "#{content.name} \n\n レート: \n #{content.rating} \n\n 営業時間: #{output_times(content)}" 
+      text: "#{content.name} \n\n レート: \n #{content.rating}" 
      }
     end
   end
 
-  def self.output_times(content)
-    if content.deals.time_start && content.deals.time_end
-      "#{content.deals["time_start"]} ~ #{content.deals["time_end"]}"
-    else 
-      "なし"
-    end
-  end
 end
 
