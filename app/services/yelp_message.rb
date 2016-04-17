bcd7db06449f6aebedbba76b7ea8a2e16f6e7e4f
山田　潤 <junyamada107@gmail.com>
2016-04-18 00:31:12 +0900

    build address

--- app/services/yelp_message.rb
+++ app/services/yelp_message.rb
@@ -3,9 +3,13 @@ class YelpMessage
     res.businesses.map do |content|
      {  
       contentType: LineContent::TEXT,
-      text: "#{content.name} \n\n レート: \n #{content.rating} \n\n 住所: #{content.location.address.first}" 
+      text: "#{content.name} \n\n レート: #{content.rating} \n\n 住所: #{build_address(content.location.display_address)}" 
      }
     end
+  end
+
+  def self.build_address(display_address)
+    display_address.reverse.join
   end
 end
 
