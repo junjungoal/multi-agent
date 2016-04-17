0bd55c36dd119d77260ff3e60dbe9ee2fc204f11
山田　潤 <junyamada107@gmail.com>
2016-04-17 16:32:35 +0900

    fix text

--- app/services/yelp_message.rb
+++ app/services/yelp_message.rb
@@ -3,7 +3,7 @@ class YelpMessage
     res.businesses.map do |content|
      {  
       contentType: LineContent::TEXT,
-      text: "名前 : #{content.name} \n レート: #{content.rating} \n url: #{content.url}" 
+      text: " 名前 : #{content.name} \n レート: #{content.rating} \n url: #{content.url}" 
      }
     end
   end
